3e26ce6017ecd898be9dd94bb2f9ae2e16579a2f
erichydrick <eric.hydrick@gmail.com>
2024-07-27 00:58:30 -0400

    Updated conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,17 +1,17 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        opts = {},
-        config = function()
-            local conform = require("conform")
-            conform.setup({
-                formatters_by_ft = {
-                    javascript = { { "prettierd", "prettier" } },
-                    lua = { "stylua" }
-                },
-                format_on_save = { timeout_ms = 500, lsp_fallback = true }
-            })
-            -- TODO: SET UP A KEYBINDING TO MANUALLY FORMAT THE FILE (<LEADER>GF)
-        end,
-    },
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { { "prettierd", "prettier" } },
+					lua = { "stylua" },
+				},
+				format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			})
+			-- TODO: SET UP A KEYBINDING TO MANUALLY FORMAT THE FILE (<LEADER>GF)
+		end,
+	},
 }
