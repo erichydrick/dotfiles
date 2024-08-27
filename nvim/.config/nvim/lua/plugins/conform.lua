991e22f388fd0cafef5d16827ac894a5a8a61934
Hydrick, Eric <Hydrice@LabCorp.com>
2024-08-27 17:00:32 -0400

    Updated Neovim plugins, working on fixing Dadbod.

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,17 +1,18 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function()
-			local conform = require("conform")
-			conform.setup({
-				formatters_by_ft = {
-					javascript = { "prettierd", "prettier", stop_after_first = true },
-					lua = { "stylua" },
-				},
-				format_on_save = { timeout_ms = 500, lsp_fallback = true },
-			})
-			-- TODO: SET UP A KEYBINDING TO MANUALLY FORMAT THE FILE (<LEADER>GF)
-		end,
-	},
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        config = function()
+            local conform = require("conform")
+            conform.setup({
+                formatters_by_ft = {
+                    javascript = { "prettierd", "prettier", stop_after_first = true },
+                    lua = { "stylua" },
+                    yaml = { "yamlfix" }
+                },
+                format_on_save = { timeout_ms = 500, lsp_fallback = true },
+            })
+            -- TODO: SET UP A KEYBINDING TO MANUALLY FORMAT THE FILE (<LEADER>GF)
+        end,
+    },
 }
