df0babdeb88cf4804d9efff4064514ee2973fd58
Eric Hydrick <eric.hydrick@gmail.com>
2025-04-01 22:43:10 -0400

    Trying a font with ligatures, and Ghostty too

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -20,12 +20,6 @@ return {
             require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
-                snippet = {
-                    -- REQUIRED - you must specify a snippet engine
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                    end,
-                },
                 window = {
                     -- completion = cmp.config.window.bordered(),
                     -- documentation = cmp.config.window.bordered(),
