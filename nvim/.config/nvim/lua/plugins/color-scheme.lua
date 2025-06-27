1645a8411dc71bb5093af125b523df7554f328e6
Eric Hydrick <eric.hydrick@gmail.com>
2025-06-27 01:17:21 -0400

    Color scheme cleanup

--- nvim/.config/nvim/lua/plugins/color-scheme.lua
+++ nvim/.config/nvim/lua/plugins/color-scheme.lua
@@ -1,11 +1,3 @@
-function ColorMyPencils(color)
-    color = color or "kanagawa"
-    vim.cmd.colorscheme(color)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#000000" })
-end
-
 return {
     {
         "rebelot/kanagawa.nvim",
@@ -19,10 +11,9 @@ return {
                     dark = "dragon",
                     light = "lotus",
                 },
-                transparent = false,
+                transparent = true,
             })
-            ColorMyPencils("kanagawa")
-            -- vim.cmd("colorscheme kanagawa")
+            vim.cmd("colorscheme kanagawa")
         end,
     }
 }
