59d04846a49d4a071579d829ac15953584ccab5f
Eric Hydrick <eric@optimantra.com>
2026-02-13 23:05:48 -0500

    Minor upgrades
    Fixed conform error
    Added Dadbod because MySQL wasn't running on save
    Fixed popups still appearing in the bottom right

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,27 @@
 return {
     {
         "stevearc/conform.nvim",
-        opts = {},
-        config = function()
-            local conform = require("conform")
-            conform.setup({
-                formatters_by_ft = {
-                    java = {
-                        "spotless_gradle",
-                        "google-java-format",
-                        stop_after_first = true,
-                    },
-                    javascript = {
-                        "prettierd",
-                        "prettier",
-                        stop_after_first = true,
-                    },
-                    lua = { "stylua" },
-                    yaml = { "yamlfix" },
+        opts = {
+            formatters_by_ft = {
+                java = {
+                    "spotless_gradle",
+                    "google-java-format",
+                    stop_after_first = true,
                 },
-                format_on_save = nil,
-                vim.keymap.set(
-                    "n",
-                    "<leader>cf",
-                    "conform.format({ async = true })"
-                ),
-            })
-        end,
+                javascript = {
+                    "prettierd",
+                    "prettier",
+                    stop_after_first = true,
+                },
+                lua = { "stylua" },
+                yaml = { "yamlfix" },
+            },
+            format_on_save = { timeout_ms = 500, lsp_fallback = true },
+            vim.keymap.set(
+                "n",
+                "<leader>cf",
+                "conform.format({ async = true })"
+            ),
+        },
     },
 }
