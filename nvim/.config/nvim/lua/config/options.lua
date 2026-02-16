91f859183c99ec5e5a8e7f4e8bdc68fd018f661c
Eric Hydrick <eric.hydrick@gmail.com>
2026-02-16 00:03:00 -0500

    Got the status column the way I wanted it
    Finally saw s9tpepper on Twitch to get the username and find the
    dotfiles. Adjusted for personal style. Will likely want to tweak more to
    make dynamic with theme switching, but that's a long ways off.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -18,7 +18,47 @@ opt.shiftwidth = 4
 opt.smartcase = true
 opt.splitbelow = true
 opt.splitright = true
-opt.statuscolumn = "%{v:relnum} %{v:lnum}"
+-- #76946A
+-- vim.api.nvim_set_hl(0, "StatusColumnLineNr", { fg = "#585b70", bg = "NONE" })
+vim.api.nvim_set_hl(
+    0,
+    "StatusColumnLineNrCursor",
+    { fg = "#98BB6C", bg = "NONE", reverse = false }
+)
+-- Thanks to s9tpepper and his dotfiles (https://github.com/s9tpepper/dotfiles/blob/13c4c4fa9f6135a94128e08d5d4fe7657ad1d14c/neovim/.config/nvim/lua/after/after.lua#L30)
+
+vim.o.statuscolumn = table.concat({
+    "%@SignCb@", -- Clickable sign column
+    "%s", -- Sign column
+    "%=", -- Right align
+
+    -- Relative line number (current line)
+    "%#StatusColumnLineNrCursor#",
+    "%-1{v:virtnum == 0 ? (v:relnum == 0 ? v:relnum : '') : ''}",
+
+    -- Relative line number (non-current line)
+    "%#StatusColumnLineNr#",
+    "%-1{v:virtnum == 0 ? (v:relnum == 0 ? '' : v:relnum) : ''}",
+
+    -- Padding space (current line)
+    "%#StatusColumnLineNrCursor#",
+    "%{v:virtnum == 0 ? (v:relnum == 0 ? ' ' : '') : ''}",
+
+    -- Padding space (non-current line)
+    "%#StatusColumnLineNr#",
+    "%{v:virtnum == 0 ? (v:relnum == 0 ? '' : ' ') : ''}",
+
+    -- Absolute line number (current line)
+    "%#StatusColumnLineNrCursor#",
+    "%{v:virtnum == 0 ? (v:lnum == line('.') ? v:lnum : '') : ''}",
+
+    -- Absolute line number (non-current line)
+    "%#StatusColumnLineNr#",
+    "%{v:virtnum == 0 ? (v:lnum == line('.') ? '' : v:lnum) : ''}",
+
+    "│ ", -- Separator
+})
+
 opt.tabstop = 4
 
 -- My options
