af7c606c87ac7392e64c60d7233e9f43d9ea762c
Eric Hydrick <eric.hydrick@gmail.com>
2026-03-23 09:21:07 -0400

    My snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,16 +1,23 @@
 return {
     "snacks.nvim",
     opts = {
+        bigfile = { enabled = true },
         explorer = { enabled = false },
         indent = { enabled = false },
         input = { enabled = false },
         notifier = { enabled = false },
+        picker = { enabled = false },
+        quickfile = { enabled = true },
         scope = { enabled = false },
         scroll = { enabled = false },
         statuscolumn = { enabled = false }, -- we set this in options.lua
-        words = { enabled = false },
-        bigfile = { enabled = true },
-        quickfile = { enabled = true },
         terminal = { enabled = false },
+        words = { enabled = false },
+    },
+    keys = {
+        { "<leader>fe", false },
+        { "<leader>fE", false },
+        { "<leader>e", false },
+        { "<leader>E", false },
     },
 }
