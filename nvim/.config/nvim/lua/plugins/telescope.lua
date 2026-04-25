f7781d09c93892c9ab4093b9ae444ce1df5f0f4f
Eric Hydrick <eric.hydrick@gmail.com>
2026-04-25 01:33:38 -0400

    Starting to level up Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,27 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "sharkdp/fd" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sharkdp/fd",
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		},
 		config = function()
+			require("telescope").setup({
+				pickers = {
+					live_grep = {
+						layout_config = {
+							width = 0.9,
+						},
+						theme = "dropdown",
+					},
+				},
+			})
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fn", function()
+				builtin.find_files({ cwd = vim.fn.stdpath("config") })
+			end, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
